bd59babde6ec696b31b35bb33dd63a45713f6946
Allan Wafula <allanwafula2017@gmail.com>
2024-12-16 14:41:18 +0300

    On branch main

--- resources/views/admin/admin_profile.blade.php
+++ resources/views/admin/admin_profile.blade.php
@@ -1,6 +1,8 @@
 @extends('admin.admin_dashboard')
 
 @section('admin')
+ 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
 <div class="page-content">
     <div class="container-fluid">
@@ -58,6 +60,61 @@
                 <!-- end card -->
 
 
+            <div class="card-body p-4">
+                <form action="">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div>
+                                <div class="mb-3">
+                                    <label for="example-text-input" class="form-label">Name</label>
+                                    <input class="form-control" name="name" type="text" value="{{$profileData->name }}" id="example-text-input">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="example-text-input" class="form-label">Email</label>
+                                    <input class="form-control" name="email" type="email" value="{{$profileData->email }}" id="example-text-input">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="example-text-input" class="form-label">Phone</label>
+                                    <input class="form-control" name="phone" type="text" value="{{$profileData->phone }}" id="example-text-input">
+                                </div>
+
+
+
+
+
+
+
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6">
+
+                                <div class="mb-3">
+                                    <label for="example-text-input" class="form-label">Address</label>
+                                    <input class="form-control" name="address" type="text" value="{{$profileData->address }}" id="example-text-input">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="example-text-input" class="form-label">Profile Image</label>
+                                    <input class="form-control" name="photo" type="file"  id="image">
+                                </div>
+
+                                <div class="mb-3">
+                                    <img id="showImage" src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg')}}" alt="" class="rounded-circle p-1 bg-primary" width="110">
+                                </div>
+
+                            <div class="mt-4">
+                                <button type="submit" class="btn btn-primary waves-effect waves-light">Save Changes</button>
+                            </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
                 <!-- end tab content -->
             </div>
             <!-- end col -->
@@ -69,5 +126,17 @@
 
     </div> <!-- container-fluid -->
 </div>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('image').change(function(e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#showImage').attr('src',e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        })
+    })
+</script>
 
 @endsection
